f74bfaa3c8203d4ba21215148cf990aa6c055075
valentcap <valent.christian@ti.ukdw.ac.id>
2019-09-14 06:22:22 +0700

    + tab pada home untuk tampilan 2 fragment

---
+++ app/src/main/java/com/example/layout2/ViewPagerAdapter.java
@@ -0,0 +1,35 @@
+package com.example.layout2;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private Fragment[] childFragments;
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+        childFragments = new Fragment[]{
+                new fragment_menu(),
+                new fragment_menu2()
+        };
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return childFragments[position];
+    }
+
+    @Override
+    public int getCount() {
+        return childFragments.length;
+        //three fragments
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title = getItem(position).getClass().getName();
+        return title.subSequence(title.lastIndexOf(".") + 1, title.length());
+    }
+}
--- app/src/main/java/com/example/layout2/activity_home_page.java
+++ app/src/main/java/com/example/layout2/activity_home_page.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.View;
 import android.widget.EditText;
@@ -21,21 +23,30 @@ public class activity_home_page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         Intent intent = getIntent();
-        prepareFragment();
+
+
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
+
+
+//        TabLayout tabLayout = findViewById(R.id.tab_layout);
+//        tabLayout.setupWithViewPager(viewPager);
+
+        //prepareFragment();
 
 
     }
 
+    //tugas sebelumnya
+//    public void toAbout(View view) {
+//        Intent intent = new Intent(this, About.class);
+//        //EditText editText = (EditText) findViewById(R.id.txtEmail);
+//        startActivity(intent);
+//    }
 
-    public void toAbout(View view) {
-        Intent intent = new Intent(this, About.class);
-        //EditText editText = (EditText) findViewById(R.id.txtEmail);
-        startActivity(intent);
-    }
-
-    private void prepareFragment(){
-        this.getSupportFragmentManager().beginTransaction()
-                .add(R.id.fragment_pakai_menu, new fragment_menu()).commit();
-    }
+//    private void prepareFragment(){
+//        this.getSupportFragmentManager().beginTransaction()
+//                .add(R.id.fragment_pakai_menu, new fragment_menu()).commit();
+//    }
 
 }
